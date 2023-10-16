ec005c2f16dde54bab182d3c8407791c09709e47
YOKOYAMA Taku <seekseep@gmail.com>
2023-10-16 15:24:15 +0900

    [ADD] adsence

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,10 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body>{children}</body>
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1057611392436338"
+        crossOrigin="anonymous" />
     </html>
   )
 }
