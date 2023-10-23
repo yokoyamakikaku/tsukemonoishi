63cf7f9447df23875b22bf49e30671e9ca28ac3f
YOKOYAMA Taku <seekseep@gmail.com>
2023-10-23 10:35:34 +0900

    [ADD] Google Ads meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout ({
   return (
     <html lang="ja">
       <head>
+      <meta name="google-adsense-account" content="ca-pub-1057611392436338">
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1057611392436338"
